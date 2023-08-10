87be53e28061dfa12cf76c1c42b7c39dd7d4211d
mal1ing <ghfk1235@naver.com>
2023-08-11 00:44:23 +0900

    데이터 넣기 삭제 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -22,4 +23,25 @@ public class BoardController {
         boardService.write(board);
         return "";
     }
+
+    @GetMapping("/board/list")
+    public String boardlist(Model model) {
+        //데이터를 담아 페이지로 보내기 위해 Model 자료형을 인자로
+        model.addAttribute("list", boardService.boardlist());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardview(Model model, Integer id){
+        model.addAttribute("board", boardService.boardview(id));
+        return "boardview";
+    }
+
+    //게시글 삭제
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -14,4 +16,20 @@ public class BoardService {
     public void write(Board board){
         boardRepository.save(board);
     }
+
+    public List<Board> boardlist(){
+        return boardRepository.findAll();
+        //Board라는 class가 담긴 list를 찾아 반환
+    }
+
+    //특정 게시물 불러오기
+    public Board boardview(Integer id){
+        return boardRepository.findById(id).get();
+    }
+
+    // 특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+
 }
